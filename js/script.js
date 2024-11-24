1998e9ec39006ae4e4a00bb561cbaf68f2dafa7d
aliyun8925813839 <f17778475461@139.com>
2024-11-25 00:27:28 +0800

    213123

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// script.js
-
-// 确保 GSAP ScrollTrigger 已加载
+// 注册插件
 gsap.registerPlugin(ScrollTrigger);
 
-// 为每条消息创建动画
+// 为每条消息添加滚动动画
 gsap.utils.toArray('.message').forEach((message, index) => {
   gsap.fromTo(
     message,
@@ -14,9 +12,48 @@ gsap.utils.toArray('.message').forEach((message, index) => {
       duration: 0.6,
       scrollTrigger: {
         trigger: message,
-        start: "top 80%", // 元素进入视口 80% 时触发
-        toggleActions: "play none none reverse", // 滚动返回时反转动画
-      }
+        start: "top 80%",
+        toggleActions: "play none none reverse",
+      },
     }
   );
+});
+
+// 创建时间轴 (timeline) 控制滚出图片效果
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.exit-image', // 最后一部分图片触发
+    start: "top 90%", // 当到达视口底部 90% 时触发
+    toggleActions: "play none none none", // 动画只触发一次
+  },
+});
+const tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.exit-image2', // 最后一部分图片触发
+    start: "top 90%", // 当到达视口底部 90% 时触发
+    toggleActions: "play none none none", // 动画只触发一次
+  },
+});
+
+// 定义滚出动画
+tl.to('.exit-image', {
+  opacity: 1,
+  x: 50, // 向右滑动一点，增加动感
+  duration: 1.5,
+  ease: "power2.out",
+}).to('.exit-image', {
+  x: "50%", // 图片完全滚出屏幕
+  duration: 1.5,
+  ease: "power2.in",
+});
+// 定义滚出动画
+tl2.to('.exit-image2', {
+  opacity: 1,
+  x: 50, // 向右滑动一点，增加动感
+  duration: 1.5,
+  ease: "power2.out",
+}).to('.exit-image2', {
+  x: "50%", // 图片完全滚出屏幕
+  duration: 1.5,
+  ease: "power2.in",
 });
